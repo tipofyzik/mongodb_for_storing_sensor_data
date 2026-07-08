8ce98ef9021ffcb30aaba495c5801e9c1847e044
tipofyzik <84290230+tipofyzik@users.noreply.github.com>
2026-07-08 16:27:54 +0200

    Add files via upload

--- code/main.py
+++ code/main.py
@@ -30,21 +30,36 @@ n_records = 5
 update_rows = iot_data[:batch_size]
 test_rows = update_rows.sample(n=n_records)
 
-# Inserting the base columns into MongoDB in batches
-database.insert_batch(data_list=iot_data[base_columns].to_dict("records"), batch_size=batch_size)
-print("Inserted all data into MongoDB in batches of {}.".format(batch_size))
-print("\nTotal rows in the dataset: {}".format(len(iot_data)))
-print(f"Total documents in collection: {database.count_documents()}\n")
+insert = True
+if insert:
+    # Inserting the base columns into MongoDB in batches
+    database.insert_batch(data_list=iot_data[base_columns].to_dict("records"), batch_size=batch_size)
+    print("Inserted all data into MongoDB in batches of {}.".format(batch_size))
+    print("\nTotal rows in the dataset: {}".format(len(iot_data)))
+    print(f"Total documents in collection: {database.count_documents()}\n")
 
-# Checking the latest records in MongoDB
-print (f"{n_records} random  records before adding extra columns")
-for i, last_record in enumerate(database.get_by_keys(keys = list(zip(test_rows["ts"], test_rows["device"])))):
-    print(f"Record {i+1}: {last_record}")
+    # Checking the latest records in MongoDB
+    print (f"{n_records} random  records before adding extra columns")
+    for i, last_record in enumerate(database.get_by_keys(keys = list(zip(test_rows["ts"], test_rows["device"])))):
+        print(f"Record {i+1}: {last_record}")
 
-# Adding extra columns to the existing records in MongoDB
-database.update_batch(update_rows[extra_columns].to_dict("records"))
+update = True
+if update:
+    # Adding extra columns to the existing records in MongoDB
+    database.update_batch(update_rows[extra_columns].to_dict("records"))
 
-# Checking the latest records in MongoDB
-print (f"\n{n_records} random records after adding extra columns")
-for i, last_record in enumerate(database.get_by_keys(keys = list(zip(test_rows["ts"], test_rows["device"])))):
-    print(f"Record {i+1}: {last_record}")
+    # Checking the latest records in MongoDB
+    print (f"\n{n_records} random records after adding extra columns")
+    for i, last_record in enumerate(database.get_by_keys(keys = list(zip(test_rows["ts"], test_rows["device"])))):
+        print(f"Record {i+1}: {last_record}")
+
+# Alternatively, instead of updating existing documents, we can simply add new ones, 
+# preserving timestamps and device identifiers, which will help in matching new data with the old one.
+alternative = False
+if alternative:
+    database.insert_batch(data_list=update_rows[extra_columns].to_dict("records"), batch_size=batch_size)
+
+    # Checking the latest records in MongoDB
+    print (f"\n{n_records} random records after adding extra columns")
+    for i, last_record in enumerate(database.get_by_keys(keys = list(zip(test_rows["ts"], test_rows["device"])))):
+        print(f"Record {i+1}: {last_record}")
